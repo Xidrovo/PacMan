d44449fb2c17629b3a86c627096ede480f876a1d
Xidrovo <Xidrovo@hotmail.com>
2015-10-07 22:08:21 -0500

    Menu
    Menusito

---
+++ Pacman/Assets/Scripts/General/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public Animator Quit, start;
+
+    public void StartClicked()
+    {
+        start.SetBool("Pressed", true);
+        Debug.Log("Hello World");
+        Invoke("OpenScene", 1);
+    }
+
+    private void OpenScene()
+    {
+        Application.LoadLevel("Main Game");
+    }
+
+    public void QuitClicked()
+    {
+        Quit.SetBool("Pressed", true);
+    }
+}
