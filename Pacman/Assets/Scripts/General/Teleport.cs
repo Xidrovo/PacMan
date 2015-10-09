11881a61f50d1e872fcf7c2554fc3f89a131330f
Xidrovo <Xidrovo@hotmail.com>
2015-10-09 00:47:44 -0500

    Teleport
    Teleport~

---
+++ Pacman/Assets/Scripts/General/Teleport.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+    public GameObject TeleportA, TeleportB;
+
+    private Vector3 Temp;
+    void OnCollisionEnter2D(Collision2D Coll)
+    {
+        if (Coll.gameObject == TeleportA)
+        {
+            Temp = TeleportB.transform.position;
+            Temp.x = TeleportB.transform.position.x - 0.5f;
+
+            this.transform.position = Temp;
+        }
+        if (Coll.gameObject == TeleportB)
+        {
+            this.transform.position = TeleportA.transform.position;
+        }
+    }
+}
